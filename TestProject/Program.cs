dcb65590e7c3905840bbfa580b0451f55c842985
aniketkalvit <aniketkalvit@gmail.com>
2022-09-19 11:39:25 +0530

    Add project files.

---
+++ TestProject/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your percentage");
+            int per=Convert.ToInt32(Console.ReadLine()); 
+            if (per >= 75) 
+            {
+                Console.WriteLine("Grade is A+");
+            }
+            else if(per >=50 && per <75)  // range  50 - 74
+            {
+                Console.WriteLine("Grade is A");
+            }
+            else if (per >= 40 && per < 50)  // range  40 - 49
+            {
+                Console.WriteLine("Grade is B");
+            }
+            else
+            {
+                Console.WriteLine("You are Fail");
+            }
+
+            Console.WriteLine("End of program");
+        }
+    }
+}
