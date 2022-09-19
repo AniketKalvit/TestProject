9e4f16b07bf5ab09993174e0acf4052b2a5fe837
aniketkalvit <aniketkalvit@gmail.com>
2022-09-19 11:46:17 +0530

    added new file

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -8,28 +8,28 @@ namespace TestProject
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Enter your percentage");
-            int per=Convert.ToInt32(Console.ReadLine()); 
-            if (per >= 75) 
-            {
-                Console.WriteLine("Grade is A+");
-            }
-            else if(per >=50 && per <75)  // range  50 - 74
-            {
-                Console.WriteLine("Grade is A");
-            }
-            else if (per >= 40 && per < 50)  // range  40 - 49
-            {
-                Console.WriteLine("Grade is B");
-            }
-            else
-            {
-                Console.WriteLine("You are Fail");
-            }
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Enter your percentage");
+        //    int per = Convert.ToInt32(Console.ReadLine());
+        //    if (per >= 75)
+        //    {
+        //        Console.WriteLine("Grade is A+");
+        //    }
+        //    else if (per >= 50 && per < 75)  // range  50 - 74
+        //    {
+        //        Console.WriteLine("Grade is A");
+        //    }
+        //    else if (per >= 40 && per < 50)  // range  40 - 49
+        //    {
+        //        Console.WriteLine("Grade is B");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("You are Fail");
+        //    }
 
-            Console.WriteLine("End of program");
-        }
+        //    Console.WriteLine("End of program");
+        //}
     }
 }
